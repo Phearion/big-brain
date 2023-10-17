26178a17f646cffb09b8c96b4125d86611107620
PhantHive <zakaria.chaouki@ipsa.fr>
2023-10-17 15:50:54 +0200

    chore: now running with vite, eslint and husky

---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+import logo from "../logo.png";
+import React from "react";
+
+export const Header = () => {
+    return (
+        <div className="Header">
+            <img src={logo} width={250} height={250} />
+        </div>
+    )
+}
---
+++ src/components/submit.tsx
@@ -0,0 +1,7 @@
+export function SubmitBtn() {
+    return (
+        <button className="subBtn">
+            J'suis en PLS
+        </button>
+    );
+}
