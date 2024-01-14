ec5c2f32fef155dd0b7f9d3268fc7cf03d5cdccc
PCK <charles.kantane@outlook.fr>
2024-01-14 14:54:57 +0100

    chore: separation of App.css into several themed css files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { Body } from './components/Body.tsx';
 import { Footer } from './components/Footer.tsx';
 import { Header } from './components/Header.tsx';
-import './App.css';
+import './styles/common-style.css';
+import './styles/mobiles-style.css';
+import './styles/tablets-style.css';
+import './styles/large-screens-style.css';
 
 function App() {
 	// render
